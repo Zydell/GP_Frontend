d2eae9329454162bd3e4c80305183d36f35acc84
Cristhian <120286539+Zydell@users.noreply.github.com>
2024-07-04 17:25:42 -0500

    Menu user

--- src/app/ModuloUsuario/pg-dashuser/pg-dashuser.component.ts
+++ src/app/ModuloUsuario/pg-dashuser/pg-dashuser.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AuthService } from '../../ModuloServiciosWeb/Servicio.Auth';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pg-dashuser',
   templateUrl: './pg-dashuser.component.html',
-  styleUrls: [ './pg-dashuser.component.css'  ] 
+  styleUrls: [ './pg-dashuser.component.css',
+    "./../../../assets/vendor/bootstrap-icons/bootstrap-icons.css"
+    ] 
 })
 export class PgDashuserComponent {
   title = 'GreenPoint';
   constructor(public  authService: AuthService, private router: Router) {}
-    
+
+  menus: { [key: string]: boolean } = {};
+
+  toggleMenu(menu: string, event: Event) {
+    this.menus[menu] = !this.menus[menu];
+    event.stopPropagation();
+  }
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    Object.keys(this.menus).forEach(menu => {
+      this.menus[menu] = false;
+    });
+  }
+
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
