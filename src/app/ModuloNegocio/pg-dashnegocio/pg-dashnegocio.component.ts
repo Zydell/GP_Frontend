d2cf0db1ed2db8fa4ee37381aece5fa2608b0ab7
Cristhian <120286539+Zydell@users.noreply.github.com>
2024-07-18 10:41:43 -0500

    act

--- src/app/ModuloNegocio/pg-dashnegocio/pg-dashnegocio.component.ts
+++ src/app/ModuloNegocio/pg-dashnegocio/pg-dashnegocio.component.ts
@@ -42,8 +42,6 @@ export class PgDashnegocioComponent {
       console.log("PUNTOS VERDEEEEEE: "+ data)
       if (data) {
         this.cant_pvs = data.length;
-      }else{
-        this.cant_pvs = 0;
       }
     } catch (error) {
       console.error("Error obteniendo los puntos verdes del negocio: ", error);
@@ -57,12 +55,7 @@ export class PgDashnegocioComponent {
       console.log("PUNTOS VERDEEEEEE: "+ data)
       
       if (data) {
-        
-        if(data.length){
-          this.cant_ofts = data.length;
-        }else{
-          this.cant_ofts = 0;
-        }
+        this.cant_ofts = data.length;
       }
       
     } catch (error) {
